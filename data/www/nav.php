cd7570cc9042659fd3e4382aa2e048d5e2886be3
timvengust <tim.vengust@student.um.si>
2026-05-24 14:14:40 +0200

    Commit Tim - 2 del razvoja
    Commit

---
+++ data/www/nav.php
@@ -0,0 +1,22 @@
+<!-- NAVIGACIJA -->
+  <nav class="navbar navbar-expand-lg navbar-novak sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            <img src="slike/Novak_logo.png" alt="Novak Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+                <li class="nav-item"><a class="nav-link" href="index.html">Domov</a></li>
+                <li class="nav-item"><a class="nav-link" href="onas.html">O nas</a></li>
+                <li class="nav-item"><a class="nav-link" href="storitve.html">Storitve</a></li>
+                <li class="nav-item"><a class="nav-link" href="kotnakt1.html">Kontakt</a></li>
+                <li class="nav-item ms-lg-4">
+                    <a class="btn btn-black" href="kotnakt1.html">Rezerviraj termin</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
