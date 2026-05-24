76d61ab96c5dcbb15d17264da08be0e8e3e58160
tijaed <tija.edelbaher@student.um.si>
2026-05-24 19:52:49 +0200

    Razvoj 2. del

---
+++ projekt/data/www/footer.php
@@ -0,0 +1,28 @@
+<footer class="main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 mb-4">
+                <img src="slike/Novak_logo.png" alt="Logo" class="footer-logo">
+            </div>
+            <div class="col-lg-3 mb-4">
+                <h6>Naslov</h6>
+                <p>Cankarjeva ulica 4,<br>2000 Maribor</p>
+            </div>
+            <div class="col-lg-3 mb-4">
+                <h6>Kontakt</h6>
+                <p><a href="mailto:novak_zakljucnadela@gmail.com">novak_zakljucnadela@gmail.com</a></p>
+            </div>
+            <div class="col-lg-3 mb-4">
+                <h6>Sledite nam</h6>
+                <div class="social-icons">
+                    <a href="#"><img src="slike/facebook.svg" alt="Facebook"></a>
+                    <a href="#"><img src="slike/instagram.svg" alt="Instagram"></a>
+                    <a href="#"><img src="slike/youtube.svg" alt="YouTube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+            <p class="copyright-text">© 2026 Novak Zaključna Dela. Vse pravice pridržane.</p>
+        </div>
+    </div>
+</footer>
